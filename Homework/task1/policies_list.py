c428c7759e2801f33db0509553229d84c7041e63
Tetiana Pylypenko <tasya.kopayeva@gmail.com>
2023-11-10 17:12:29 +0200

    policies test update

--- Homework/task1/policies_list.py
+++ Homework/task1/policies_list.py
@@ -1,14 +1,20 @@
 from selenium import webdriver
-import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import pytest
 
-driver = webdriver.Chrome()
-driver.get('https://EPAM.com')
-driver.maximize_window()
+@pytest.mark.parametrize("browser", ["chrome", "firefox"])
 
-def test_qwe():
+def test_title(browser):
+    if browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "firefox":
+        driver = webdriver.Firefox()
+    else:
+        raise ValueError(f"Unsupported browser: {browser}")
+
+    driver.get('https://EPAM.com')
+    driver.maximize_window()
+
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     expected_policies_list1 = ["INVESTORS", "OPEN SOURCE", "PRIVACY POLICY"]
@@ -21,24 +27,11 @@ def test_qwe():
     for policy in expected_policies_list1:
         assert policy in policies_text1, f"Test Failed: {policy} is missing from the first policies list." 
 
-        # if policy not in policies_text1:
-        #     print(f"Test Failed: {policy} is missing from the first policies list.")
-        #     break
-
     # Locate and verify the second policies list
     policies_list2 = driver.find_element(By.CSS_SELECTOR, ".policies-right")
     policies_text2 = policies_list2.text
 
     for policy in expected_policies_list2:
         assert policy in policies_text2, f"Test Failed: {policy} is missing from the second policies list." 
- # if policy not in policies_text2:
-        #     print(f"Test Failed: {policy} is missing from the second policies list.")
-        #     break
-  
 
-       
-#     else:
-#         print("Test Passed: All expected policies are found in both lists.")
-
-# finally:
     driver.quit()
